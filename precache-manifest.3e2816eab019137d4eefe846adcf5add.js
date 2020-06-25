3be8da9590a1b87d393a8eea57a432905b1e89b7
easyvipin <vipinchandra555@gmail.com>
2020-06-25 20:52:36 +0530

    Updates

---
+++ precache-manifest.3e2816eab019137d4eefe846adcf5add.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8000ac17ed32d65c4ae98d5a244e76da",
+    "url": "/Expense-monitor/index.html"
+  },
+  {
+    "revision": "dfb3c824cc5033c8c54c",
+    "url": "/Expense-monitor/static/css/main.0fdbc3b3.chunk.css"
+  },
+  {
+    "revision": "3117098abe1c4bf4d40d",
+    "url": "/Expense-monitor/static/js/2.5866743a.chunk.js"
+  },
+  {
+    "revision": "6de07e5aec012398ec95763a9e7531f9",
+    "url": "/Expense-monitor/static/js/2.5866743a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb3c824cc5033c8c54c",
+    "url": "/Expense-monitor/static/js/main.12978d43.chunk.js"
+  },
+  {
+    "revision": "9ca0b93ae3c9940c7420",
+    "url": "/Expense-monitor/static/js/runtime-main.0444728a.js"
+  },
+  {
+    "revision": "a84459af5282c3551a41505de0e5ca25",
+    "url": "/Expense-monitor/static/media/bills.a84459af.jpg"
+  },
+  {
+    "revision": "b55e877b19fd9294e6e8abca1952cf1c",
+    "url": "/Expense-monitor/static/media/brand.b55e877b.jpg"
+  },
+  {
+    "revision": "89ca6dd0cacf9a3a28dcb637a9324c51",
+    "url": "/Expense-monitor/static/media/dollar.89ca6dd0.png"
+  },
+  {
+    "revision": "d0f656d263ce4678709f6eeaef74644a",
+    "url": "/Expense-monitor/static/media/grocery.d0f656d2.jpg"
+  },
+  {
+    "revision": "8eee82450d81a20b576fd9e3ba83b19d",
+    "url": "/Expense-monitor/static/media/rent.8eee8245.jpg"
+  },
+  {
+    "revision": "662367fec15e0315d30955fbc686f55c",
+    "url": "/Expense-monitor/static/media/school.662367fe.jpg"
+  }
+]);
